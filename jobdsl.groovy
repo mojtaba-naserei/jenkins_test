cb9492a9a26e8699d1643f8cb5c88786a3054f09
Mojtaba Naseri <mojtaba.naserei@gmail.com>
2019-12-23 22:53:39 +0330

    Create jobdsl.groovy

---
+++ jobdsl.groovy
@@ -0,0 +1,14 @@
+job('DSL-Tutorial-1-Test') {
+    scm {
+        git('git@github.com:contentful/the-example-app.nodejs.git')
+    }
+    triggers {
+        scm('H/15 * * * *')
+    }
+    wrappers { 
+       nodejs('nodejs8')
+    }
+    steps {
+        shell('npm install')   
+    }
+}
