4d147a30ae9c0335816fc6327e031ef80ca6d8ae
Mojtaba Naseri <mojtaba.naserei@gmail.com>
2019-12-26 13:23:47 +0330

    Add forceTag to avoid ‘ unknown flag: --force’

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -14,6 +14,7 @@ job('DSL-with-docker') {
             tag('${GIT_REVISION,length=7}')
             registryCredentials('dockerhub') // credentials id
             forcePull(false)
+            forceTag(false)
             createFingerprints(false)
             skipDecorate()
         }
