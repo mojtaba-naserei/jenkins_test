8ed99f8361c23cd6bc1d1c8a5a63b54941dbdef6
Mojtaba Naseri <mojtaba.naserei@gmail.com>
2019-12-26 11:36:43 +0330

    Add git config name and email

--- jobdsl.groovy
+++ jobdsl.groovy
@@ -1,6 +1,9 @@
 job('DSL-Tutorial-1-Test') {
     scm {
-        git('git://github.com/contentful/the-example-app.nodejs.git')
+        git('git://github.com/contentful/the-example-app.nodejs.git', 'master') { node ->
+            node / gitConfigName('mojtaba job dsl')
+            node / gitConfigEmail('mojtaba.naserei@gmail.com')
+        }
     }
     triggers {
         scm('H/15 * * * *')
